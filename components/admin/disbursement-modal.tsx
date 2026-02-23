c20eedcfccaa208858172bfcfe3660543c73ca31
gauravhotstar <hotstaer5@gmail.com>
2026-02-23 15:39:14 +0530

    Update disbursement-modal.tsx

--- components/admin/disbursement-modal.tsx
+++ components/admin/disbursement-modal.tsx
@@ -15,7 +15,7 @@ interface DisbursementModalProps {
 }
 
 // DSA Options
-const DSA_OPTIONS = ["RKPL", "Star Power", "Profincare", "DRRT"]
+const DSA_OPTIONS = ["RKPL", "Star Power", "Profincare", "DRRT", "URBAN"]
 
 export function DisbursementModal({ onSuccess }: DisbursementModalProps) {
     const [open, setOpen] = useState(false)
