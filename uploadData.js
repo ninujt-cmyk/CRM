3ea64773408897fd497f329d2af179d7a100dd07
ninujt-cmyk <ninujt@gmail.com>
2026-03-31 17:07:36 +0530

    Add uploadData.js for indexing companies in MeiliSearch

---
+++ uploadData.js
@@ -0,0 +1,31 @@
+// uploadData.js
+const { MeiliSearch } = require('meilisearch');
+const companies = require('./companies.json'); // Your test data
+
+const client = new MeiliSearch({
+  host: 'YOUR_RENDER_URL_HERE',
+  apiKey: 'YOUR_MASTER_KEY_HERE',
+});
+
+async function setupDatabase() {
+  try {
+    // 1. Create an index (think of this like a table)
+    const index = client.index('companies');
+
+    // 2. Add the documents
+    const response = await index.addDocuments(companies);
+    console.log('Upload started! Task details:', response);
+    
+    // 3. Make pincode and company_name searchable
+    await index.updateSearchableAttributes([
+      'company_name',
+      'pincode'
+    ]);
+    
+    console.log('Success! Data is indexing.');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+setupDatabase();
